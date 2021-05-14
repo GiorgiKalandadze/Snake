6b675aed8fefc93df5c9a2c36d3712e93f6175e2
GiorgiKalandadze <gkala17@freeuni.edu.ge>
2021-05-14 17:18:41 +0400

    Bound movement

--- script.js
+++ script.js
@@ -1,33 +1,42 @@
 class Config{
-    constructor(board_width, board_height, cell_size, snake_start_length, snake_x, snake_y, start_direction){
-        this.board_width = board_width;
-        this.board_height = board_height;
-        this.cell_size = cell_size;
-        this.snake_start_length = snake_start_length;
-        this.snake_x = snake_x;
-        this.snake_y = snake_y;
-        this.start_direction = start_direction;
-        
+    constructor(config){
+        this.board_x = config.board_x;
+        this.board_y = config.board_y;
+        this.board_width = config.board_width;
+        this.board_height = config.board_height;
+        this.cell_size = config.cell_size;
+        this.snake_start_length = config.snake_start_length;
+        this.snake_x = config.snake_x;
+        this.snake_y = config.snake_y;
+        this.start_direction = config.start_direction;
     }
 } 
 
 class Cell{
     //Type - Snake's part or food
     //Size - width/height (same because it's square)
-    constructor(type, cell_size, x, y, color){
-        //console.log('Constructor - Cell');
-        this.cell_size = cell_size;
+    constructor(type, size, x, y){
+        this.size = size;
         this.type = type;
         this.x = x;
         this.y = y;
-        this.color = color;
-
     }
+    //Getters
     getType(){
         return this.type;
     }
     getX(){
         return this.x;
+    }
+    getY(){
+        return this.y;
+    }
+    getSize(){
+        return this.size;
+    }
+    //Setters
+    setType(type){
+        this.type = type;
     }
     setX(x){
         this.x = x;
@@ -35,68 +44,45 @@ class Cell{
     setY(y){
         this.y = y;
     }
-    getY(){
-        return this.y;
+    setSize(size){
+        this.size = size;
     }
-    getSize(){
-        return this.cell_size;
-    }
-    getColor(){
-        return this.color;
-    }
-    getDirection(){
-        return this.direction;
-    }
-    setDirection(direction){
-        this.direction = direction;
-    }
-    
 }
 
 class Snake{
-    constructor(snake_x, snake_y, cell_size, snake_start_length, direction){
-        
-        this.snake_x = snake_x;
-        this.snake_y = snake_y;
+    constructor(x, y, cell_size, start_length, direction){
+        this.x = x;
+        this.y = y;
         this.cell_size = cell_size;
-        this.list = [];
-        this.snake_length = snake_start_length;
+        this.body_length = start_length;
         this.direction = direction;
-        this.initBody();
-        
-        
+        this.body = [];
+        this.initBody();        
     }
     initBody(){
-        for(let i = 0; i < this.snake_length; i++){
-            let current_cell = new Cell("Body", this.cell_size, this.snake_x - i * this.cell_size,  this.snake_y,  'green');
-            this.list.push(current_cell);
-            
+        for(let i = 0; i < this.body_length; i++){
+            let current_cell = new Cell("Body", this.cell_size, this.x - i * this.cell_size,  this.y);
+            this.body.push(current_cell);
         }
-        //console.log(this.list);
     }
     getBody(){
-        return this.list;
+        return this.body;
     }
-    length(){
-        return this.snake_length;
+    getLength(){
+        return this.body_length;
     }
-    head(){
-
+    getHead(){
+        return this.body[0];
     }
 }
 
 
 class Game extends Config{
-    constructor(){
-        super(500, 500, 20, 4, 450, 200, 'Right');
+    constructor(config){
+        super(config);
         this.snake = new Snake(this.snake_x, this.snake_y, this.cell_size, this.snake_start_length, this.start_direction);
-        this.new_direction = this.start_direction;
-        this.drawSnake();
-        this.changer_x = 0;
-        this.changer_y = 0;
-        
+        this.drawSnake();        
     }
-    
     drawSnake(){
         document.querySelector('.board').innerHTML = '';
         let cell, div;
@@ -106,16 +92,14 @@ class Game extends Config{
             div.className = 'cell';
             div.style.left = cell.x + "px";
             div.style.top = cell.y + "px";
-            //console.log(div.style.left);
             document.querySelector('.board').appendChild(div);
         }  
     }
     moveSnake(){
         //Move Part
-        for(let i = this.snake.getBody().length - 1; i > 0; i--){
+        for(let i = this.snake.getLength() - 1; i > 0; i--){
             let current_cell = this.snake.getBody()[i];
             let next_cell = this.snake.getBody()[i - 1];
-           
             current_cell.setX(next_cell.getX());
             current_cell.setY(next_cell.getY());
         }
@@ -133,12 +117,39 @@ class Game extends Config{
             head.x -= this.cell_size;
         }
     }
+    checkBounds(){
+        let head = this.snake.getHead();
+        if(head.getY() < this.board_y){ //Top
+            head.setY(this.board_y + this.board_height - head.getSize());
+        } else if(head.getX() + head.getSize() > this.board_x + this.board_width){ //Right
+            head.setX(this.board_x);
+        } else if(head.getY() + head.getSize() > this.board_y + this.board_height){ //Bottom
+            head.setY(this.board_y);
+        } else if(head.getX() < this.board_x){ //Left
+            head.setX(this.board_x + this.board_width - head.getSize());
+        }
+    }
+
     runGame(){
-        this.moveSnake(); //Movement of snake
+        this.moveSnake(); 
+        this.checkBounds();
         this.drawSnake();
     }
 }
 
-let game = new Game();
+let config = 
+{
+    board_x: document.querySelector('.board').offsetLeft,
+    board_y: document.querySelector('.board').offsetTop,
+    board_width: 500,
+    board_height: 500,
+    cell_size: 20, 
+    snake_start_length: 4, 
+    snake_x: document.querySelector('.board').offsetLeft + 3 * 20, 
+    snake_y: document.querySelector('.board').offsetTop, 
+    start_direction: 'Right' 
+}
+
+let game = new Game(config);
 //Need bind, otherwise 'this' in runGame is 'window' instead of 'Game
-setInterval(game.runGame.bind(game), 300);
+setInterval(game.runGame.bind(game), 100);
